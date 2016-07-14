45e0d06868bbd88baf2264b62fda62a87fd92543
Faz Ahmed <faz.ahmed@wpnchameleon.co.uk>
2016-07-14 14:53:54 +0100

    Added Name and Driving Licnese in Appication User, RegisterViewModel.

--- MovieRentalStore/MovieRentalStore/Models/AccountViewModels.cs
+++ MovieRentalStore/MovieRentalStore/Models/AccountViewModels.cs
@@ -8,6 +8,14 @@ namespace MovieRentalStore.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -65,6 +73,10 @@ namespace MovieRentalStore.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
@@ -79,6 +91,10 @@ namespace MovieRentalStore.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ResetPasswordViewModel
--- MovieRentalStore/MovieRentalStore/Models/ApplicationDbContext.cs
+++ MovieRentalStore/MovieRentalStore/Models/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MovieRentalStore.BusinessModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,14 @@ namespace MovieRentalStore.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        
+        
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
